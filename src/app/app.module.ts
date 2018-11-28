9dadc97dfd2461577b421687be46a6ce3fb932a3
Binh Tran <binhtranphuoc97@gmail.com>
2018-11-28 21:46:03 +0700

    add css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HomeComponent} from './home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [ // khai bao component
+    AppComponent,
+      HomeComponent
   ],
   imports: [
     BrowserModule,
